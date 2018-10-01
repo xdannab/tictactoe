d7229b843b5fa08d91cec8c3de4c5de9c1c135e2
Danna B <Danna@Dannas-MacBook-Pro.local>
2018-10-01 19:45:02 -0400

    put functions at the top but found error when viewing board. something about board array syntax....

--- game.py
+++ game.py
@@ -6,21 +6,26 @@ py=False
 
 
 board=[('*','A','B','C'), (1,'','',''),(2,'','',''),(3,'','','')]
-print("Welcome to Tic Tac Toe. Below is the board. To move your piece, "
-      "input a letter in caps followed by the number i.e. A1. Good Luck, and have fun.")
 
-print()
-
-for line in board:
-    print(*line);
-
-print()
-
-
-print("player X, what's your first move?")
-move=input()
+def viewb(board):
+    for line in board:
+        print(*line);
 
 def placement (move):
-    #if move[0]=='A':
-        print move[0]
+    if move[0]=='A':
+        print (viewb)
 
+
+print("Welcome to Tic Tac Toe. Below is the board. To move your piece, "
+      "input a letter in caps followed by the number i.e. A1. Good Luck, and have fun.")
+print()
+
+viewb(board)
+
+print()
+
+print("player X, what's your first move?")
+
+move=input()
+
+placement(move)
